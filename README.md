f785b89809b6e4baffbfbc52256416de14569948
Ryan Walsh <rcwalsh@gmail.com>
2025-05-09 19:13:25 -0400

    paste v1 mostly from ChatGPT

---
+++ README.md
@@ -0,0 +1,9 @@
+# Local development
+
+```bash
+npx http-server -p 8000
+```
+
+# Find sounds
+
+https://pixabay.com/sound-effects/search/laugh/
---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './style.css',
+        './sounds/laugh.mp3',
+        './sounds/cheer.mp3'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
