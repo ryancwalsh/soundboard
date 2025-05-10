9dae119451db51bb083381f8b37e0c23e465749a
Ryan Walsh <rcwalsh@gmail.com>
2025-05-09 20:51:18 -0400

    add sounds and icon

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,24 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    // If you ever update sounds or code and want to force-refresh the cache, increment the cache name to 'v2', etc.
+    caches.open('v1').then(function (cache) {
       return cache.addAll([
         './',
         './index.html',
         './manifest.json',
-        './style.css',
-        './sounds/laugh.mp3',
-        './sounds/cheer.mp3'
+        './sounds/eat.mp3',
+        './sounds/play.mp3',
+        './sounds/catio.mp3',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
