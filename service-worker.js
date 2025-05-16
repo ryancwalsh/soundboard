fe6f3c4351a3928d39b57aafb92fbba54447c13d
Ryan Walsh <rcwalsh@gmail.com>
2025-05-16 07:52:48 -0400

    add -30 and +30 buttons

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const version = 'v3'; // If you ever update sounds or code and want to force-refresh the cache, increment the cache name to 'v2', etc.
+const version = 'v4'; // If you ever update sounds or code and want to force-refresh the cache, increment the cache name to 'v2', etc.
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
